e9b747fd96f6c75e4a8e9f52e26247bff96dfd0a
bdargan <brett.dargan@gmail.com>
2013-11-23 15:44:00 +1000

    added some org-mode changes, not autoloaded yet

---
+++ dargz-pack/config/org-mode.el
@@ -0,0 +1,3 @@
+;; based on http://blog.jr0cket.co.uk/2013/08/defining-custom-workflow-with-Emacs-org-mode.html
+
+(setq org-todo-keywords '((sequence "TODO" "DOING" "BLOCKED" "REVIEW" "|" "DONE" "ARCHIVED")))
--- dargz-pack/lib/journal.el
+++ dargz-pack/lib/journal.el
@@ -1,7 +1,3 @@
-;; This buffer is for notes you don't want to save, and for Lisp evaluation.
-;; If you want to create a file, visit that file with C-x C-f,
-;; then enter the text in that file's own buffer.
-
 (setq journal-file "~/google_drives/Google Drive/core/journal.org")
 
 (defun start-journal-entry ()
@@ -9,6 +5,14 @@
   (interactive)
   (find-file journal-file)
   (goto-char (point-min))
+  (org-insert-heading)
+  (org-insert-time-stamp (current-time) t)
+  (open-line 2)
+  (insert " "))
+
+(defun add-journal-entry ()
+  "add a new journal entry."
+  (interactive)
   (org-insert-heading)
   (org-insert-time-stamp (current-time) t)
   (open-line 2)

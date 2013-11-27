d725b7dbe674102267e36333f17d7deaeb6d1c61
bdargan <brett.dargan@gmail.com>
2013-11-27 15:10:26 +1000

    set org-mode files to enable global todo list via org-todo-list commands

--- dargz-pack/config/org-mode.el
+++ dargz-pack/config/org-mode.el
@@ -1,3 +1,8 @@
 ;; based on http://blog.jr0cket.co.uk/2013/08/defining-custom-workflow-with-Emacs-org-mode.html
 
 (setq org-todo-keywords '((sequence "TODO" "DOING" "BLOCKED" "REVIEW" "|" "DONE" "ARCHIVED")))
+
+(setq
+ org-agenda-files
+ (mapcar (lambda (x) (expand-file-name x "~/google_drives/Google Drive/core/"))
+           '("journal.org" "wotif.org" "project_z.org")))
--- dargz-pack/lib/journal.el
+++ dargz-pack/lib/journal.el
@@ -1,5 +1,10 @@
 (setq journal-file "~/google_drives/Google Drive/core/journal.org")
 
+(defun unix-epoch-to-iso ()
+  (interactive)
+  (insert (format-time-string "%Y-%m-%d" (seconds-to-time (/  1384911507344 1000))))
+  )
+
 (defun start-journal-entry ()
   "Start a new journal entry."
   (interactive)
@@ -9,6 +14,8 @@
   (org-insert-time-stamp (current-time) t)
   (open-line 2)
   (insert " "))
+
+;;; insert a week  (insert (concat "**" (format-time-string "%Y-W%W" (current-time) t)) )
 
 (defun add-journal-entry ()
   "add a new journal entry."
